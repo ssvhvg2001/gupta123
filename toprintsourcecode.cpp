4bee66e92e8863602ae9606b782f0fd7aeb6830b
ssvhvg2001 <54387422+ssvhvg2001@users.noreply.github.com>
2019-08-24 20:33:29 +0530

    Update toprintsourcecode.cpp

--- toprintsourcecode.cpp
+++ toprintsourcecode.cpp
@@ -3,33 +3,36 @@
 using namespace std;
 main()
 {
-	int x1=0;
+	int x1=0,i=0,z,z1;
 	float y;
 	char c;
 	if(x1!=0)
 	cout<<"harsha\n";
-	ifstream x("toprintsourcecode.cpp");
+	ifstream x("toprintsourcecode_2.cpp");
 	string s;
 	while(getline(x,s))
 	{
+		i=0;z=s[0];z1=s[1];
 		cout<<s;
-		if(s[1]=='i'&&s[2]=='f'&&s[3]=='(')
-		cout<<"\t This is a decision making statement \n";
+		if(s[i]=='\t')
+		for(i=0;s[i]=='\t';i++);
+		if(s[i]=='i'&&s[i+1]=='f'&&s[i+2]=='(')
+		cout<<"\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t This is a decision making statement \n";
 		else
-		if(s[1]=='i'&&s[2]=='n'&&s[3]=='t')
-		cout<<"\t This is a declararation statement for int \n";
+		if(s[i]=='i'&&s[i+1]=='n'&&s[i+2]=='t')
+		cout<<"\t\t\t\t\t\t\t\t\t\t\t This is a declararation statement for int \n";
 		else
-		if(s[1]=='f'&&s[2]=='o'&&s[3]=='r')
-		cout<<"\t This is a for loop \n";
+		if(s[i]=='f'&&s[i+1]=='o'&&s[i+2]=='r')
+		cout<<"\t\t\t\t\t\t\t\t\t This is a for loop \n";
 		else
-		if(s[1]=='f'&&s[2]=='l'&&s[3]=='o'&&s[4]=='a'&&s[5]=='t')
-		cout<<"\t This is a declaration for float \n";
+		if(s[i]=='f'&&s[i+1]=='l'&&s[i+2]=='o'&&s[i+3]=='a'&&s[i+4]=='t')
+		cout<<"\t\t\t\t\t\t\t\t\t\t\t\t This is a declaration for float \n";
 		else
-		if(s[1]=='c'&&s[2]=='h'&&s[3]=='a'&&s[4]=='r')
-		cout<<"\t\t This is a declaration for char \n";
+		if(s[i]=='c'&&s[i+1]=='h'&&s[i+2]=='a'&&s[i+3]=='r')
+		cout<<"\t\t\t\t\t\t\t\t\t\t\t\t\t This is a declaration for char \n";
 		else
-		if(s[1]=='w'&&s[2]=='h'&&s[3]=='i'&&s[4]=='l'&&s[5]=='e')
-		cout<<"\t This is a while loop this performe iteration \n";
+		if(s[i]=='w'&&s[i+1]=='h'&&s[i+2]=='i'&&s[i+3]=='l'&&s[i+4]=='e')
+		cout<<"\t\t\t\t\t\t\t\t\t\t\t This is a while loop this performe iteration \n";
 		else
 		cout<<"\n";
 	}
